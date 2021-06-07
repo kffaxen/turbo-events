0554f79c0b318c342bfd2cd385c18250e677f565
Karl-Filip Faxén <kff@sics.se>
2021-06-07 16:37:54 +0200

    Add a simple library.

---
+++ lib/turboevents.cpp
@@ -0,0 +1,9 @@
+#include "turboevents.hpp"
+
+#include <iostream>
+
+namespace TurboEvents {
+
+TurboEvents::TurboEvents(void) { std::cout << "TurboEvents initialized\n"; }
+
+} // namespace TurboEvents
