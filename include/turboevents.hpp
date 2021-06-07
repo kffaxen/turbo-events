a1a1117ce7b86e1c5477e390a38a86692ed0e299
Karl-Filip Faxén <kff@sics.se>
2021-06-07 16:37:54 +0200

    Add a simple library.

---
+++ include/turboevents.hpp
@@ -0,0 +1,11 @@
+#ifndef TURBOEVENTS_H
+#define TURBOEVENTS_H
+
+namespace TurboEvents {
+class TurboEvents {
+public:
+  TurboEvents(void);
+};
+} // namespace TurboEvents
+
+#endif
--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,8 @@
+#include "turboevents.hpp"
+
 int main(int argc, char **argv) {
   // This program does very little.
+  TurboEvents::TurboEvents turbo;
+
   return 0;
 }
