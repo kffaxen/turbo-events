1fba3273014cd021bd12ae9acb308eb9bc44781e
Karl-Filip Faxén <kff@sics.se>
2021-06-10 17:26:20 +0200

    Add documentation markup comments to include file.

--- include/turboevents.hpp
+++ include/turboevents.hpp
@@ -2,10 +2,14 @@
 #define TURBOEVENTS_H
 
 namespace TurboEvents {
+
+/// A class encapsulating an event generator
 class TurboEvents {
 public:
+  /// Simple constructor
   TurboEvents(void);
 };
+
 } // namespace TurboEvents
 
 #endif
