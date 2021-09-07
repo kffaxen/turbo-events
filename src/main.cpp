26cdca8aa9f357b82f1fc8081ff45e4207e9ea83
Peter A. Jonsson <jonsson.peter.a@gmail.com>
2021-09-07 10:51:50 +0200

    cmake: add support for gflags
    This adds support for using gflags to parse
    command line options. The library is BSD-3
    licensed and there is a Ubuntu package
    available.

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,15 @@
 #include "turboevents.hpp"
 
+#include <gflags/gflags.h>
+
 int main(int argc, char **argv) {
+  gflags::SetUsageMessage("fast event generator");
+  gflags::SetVersionString("0.1");
+  gflags::ParseCommandLineFlags(&argc, &argv, true);
+
   // This program does very little.
   TurboEvents::TurboEvents turbo;
 
+  gflags::ShutDownCommandLineFlags();
   return 0;
 }
