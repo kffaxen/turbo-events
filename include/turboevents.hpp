53075d4beb0988b13bbb790a0dcf4673e7b63ba4
Peter A. Jonsson <jonsson.peter.a@gmail.com>
2021-09-15 12:16:22 +0200

    Add create method to TurboEvents
    This encapsulates the memory management
    inside TurboEvents so the client can
    call create and treat the return value
    like a stack-allocated object.
    
    This change is preparation for handling
    faulty/incompatible command line parameters.

--- include/turboevents.hpp
+++ include/turboevents.hpp
@@ -1,6 +1,8 @@
 #ifndef TURBOEVENTS_HPP
 #define TURBOEVENTS_HPP
 
+#include <memory>
+
 namespace TurboEvents {
 
 /// A class encapsulating an event generator
@@ -8,6 +10,8 @@ class TurboEvents {
 public:
   /// Simple constructor
   TurboEvents();
+  /// Create a new TurboEvents object.
+  static std::unique_ptr<TurboEvents> create();
 };
 
 } // namespace TurboEvents
--- lib/turboevents.cpp
+++ lib/turboevents.cpp
@@ -6,4 +6,8 @@ namespace TurboEvents {
 
 TurboEvents::TurboEvents() { std::cout << "TurboEvents initialized\n"; }
 
+std::unique_ptr<TurboEvents> TurboEvents::create() {
+  return std::make_unique<TurboEvents>();
+}
+
 } // namespace TurboEvents
