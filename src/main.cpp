ebce27118b7bbdfa77147c45054f785fd7450de6
Peter A. Jonsson <jonsson.peter.a@gmail.com>
2021-09-17 12:43:52 +0200

    cmake: add Xerces detection support
    This adds the detection of Xerces to cmake
    and calls the Initialize/Terminate methods
    in main.
    
    The library will be used in a future patch.

--- src/main.cpp
+++ src/main.cpp
@@ -1,14 +1,24 @@
 #include "turboevents.hpp"
 
 #include <gflags/gflags.h>
+#include <xercesc/util/PlatformUtils.hpp>
+
+using namespace xercesc;
 
 int main(int argc, char **argv) {
   gflags::SetUsageMessage("fast event generator");
   gflags::SetVersionString("0.1");
   gflags::ParseCommandLineFlags(&argc, &argv, true);
 
+  try {
+    XMLPlatformUtils::Initialize();
+  } catch (const XMLException &e) {
+    return 1;
+  }
+
   auto turbo = TurboEvents::TurboEvents::create();
 
+  XMLPlatformUtils::Terminate();
   gflags::ShutDownCommandLineFlags();
   return 0;
 }
