6292013950141e86e008df184ff44656b6bf52b4
Karl-Filip Faxén <kff@sics.se>
2021-11-19 17:05:13 +0100

    Add classes for events and event streams.

--- include/turboevents.hpp
+++ include/turboevents.hpp
@@ -1,9 +1,30 @@
 #ifndef TURBOEVENTS_HPP
 #define TURBOEVENTS_HPP
 
+#include <chrono>
 #include <memory>
 
 namespace TurboEvents {
+
+/// A type for events with time stamps
+template <class T> struct Event {
+  /// The time stamp of the event
+  std::chrono::system_clock::time_point time;
+  /// The event data
+  T val;
+};
+
+/// A class for event streams
+template <class T> class EventStream {
+public:
+  /// Get next event
+  inline Event<T> getNext(void) { return next; }
+  /// Generate the next event and write it to next
+  virtual void generate(void) = 0;
+
+private:
+  Event<T> next;
+};
 
 /// A class encapsulating an event generator
 class TurboEvents {
