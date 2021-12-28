af422529dd67979f4dfd92373e667c9483557ad8
Peter A. Jonsson <jonsson.peter.a@gmail.com>
2021-12-28 12:07:10 +0100

    Add Kafka support
    This adds a basic event that sends Kafka
    messages when triggered.
    
    The event trigger sets up the Kafka connection
    and uses a hardcoded configuration to so the
    input side has something to do with its input.
    This commit does not add anything that uses the
    object. These limitations will be lifted in later
    commits.

---
+++ lib/IO/KafkaOutput.hpp
@@ -0,0 +1,14 @@
+#include "turboevents.hpp"
+
+namespace TurboEvents {
+
+/// Class for sending Kafka events.
+class KafkaOutput : public Event {
+  /// The data to send.
+  std::string data;
+  KafkaOutput(std::chrono::system_clock::time_point t, std::string d)
+      : Event(t), data(d) {}
+  void trigger() const override;
+};
+
+} // namespace TurboEvents
