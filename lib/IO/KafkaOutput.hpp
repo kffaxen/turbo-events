9504671ebd457169bc4d589198ba80d6fa0d7fc5
Karl-Filip Faxén <kff@sics.se>
2022-02-02 08:43:57 +0100

    Rename KafkaOutput to KafkaEvent for consistency.

--- lib/IO/KafkaOutput.hpp
+++ lib/IO/KafkaOutput.hpp
@@ -7,10 +7,10 @@
 namespace TurboEvents {
 
 /// Class for sending Kafka events.
-class KafkaOutput : public Event {
+class KafkaEvent : public Event {
   /// The data to send.
   std::string data;
-  KafkaOutput(std::chrono::system_clock::time_point t, std::string d)
+  KafkaEvent(std::chrono::system_clock::time_point t, std::string d)
       : Event(t), data(d) {}
   void trigger() const override;
 };
