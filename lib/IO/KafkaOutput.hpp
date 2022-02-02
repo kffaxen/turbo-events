8ee151b375b285d062abb29c5ccdb7a36e0cf81b
Karl-Filip Faxén <kff@sics.se>
2022-02-02 09:03:07 +0100

    Make some member functions public in KafkaEvent.

--- lib/IO/KafkaOutput.hpp
+++ lib/IO/KafkaOutput.hpp
@@ -8,11 +8,17 @@ namespace TurboEvents {
 
 /// Class for sending Kafka events.
 class KafkaEvent : public Event {
-  /// The data to send.
-  std::string data;
+public:
+  /// Public constructor
   KafkaEvent(std::chrono::system_clock::time_point t, std::string d)
       : Event(t), data(d) {}
+
+  /// Implementation of trigger() for Kafka events
   void trigger() const override;
+
+private:
+  /// The data to send.
+  std::string data;
 };
 
 } // namespace TurboEvents
